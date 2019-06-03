5952abb4ac935ae33716d9a7065127e530b5dd06
Ricardo <dobuiles@gmail.com>
2019-06-03 12:52:17 -0500

    Mejoras en show exercise

--- resources/views/exercises/show.blade.php
+++ resources/views/exercises/show.blade.php
@@ -13,33 +13,22 @@
 
         <div class="card">
 
-        @if($file['type'] == "image")
-
-            <img class="card-img-top" src='../storage/{{ $exercise->id }}/{{ $file->url }}' class="img-thumbnail"/>
-        
-        @elseif($file['type'] == "audio")
-
-            <audio controls>
-                <source class="card-img-top" src='../storage/{{ $exercise->id }}/{{ $file->url }}' type="audio/{{$file['extension']}}">
-                Your browser does not support the audio tag.
-            </audio>
-
-        @elseif($file['type'] == "video")
-
-        <div class="ard-img-top embed-responsive">
-            <iframe class="embed-responsive-item" src='../storage/{{ $exercise->id }}/{{ $file->url }}' allowfullscreen></iframe>
+            @if($file['type'] == "image")
+                <img class="card-img-top" src='../storage/{{ $exercise->id }}/{{ $file->url }}' class="img-thumbnail"/>
+            @elseif($file['type'] == "audio")
+                <audio controls>
+                    <source class="card-img-top" src='../storage/{{ $exercise->id }}/{{ $file->url }}' type="audio/{{$file['extension']}}">
+                    Your browser does not support the audio tag.
+                </audio>
+            @elseif($file['type'] == "video")
+                <div class="ard-img-top embed-responsive">
+                    <iframe class="embed-responsive-item" src='../storage/{{ $exercise->id }}/{{ $file->url }}' allowfullscreen></iframe>
+                </div>
+            @else
+                <embed class="card-img-top" src='../storage/{{ $exercise->id }}/{{ $file->url }}' />
+            @endif
+   
         </div>
-
-        @else
-
-            <embed class="card-img-top" src='../storage/{{ $exercise->id }}/{{ $file->url }}' />
-
-        @endif
-
-          
-        </div>
-
-
 
     @endforeach
 
